f760d236a404253ea7d88ba521f6233fa0817af8
RadoKopacha <petyrnikolov0804@gmail.com>
2024-02-25 23:03:15 +0200

    neshto

---
+++ CombinatingLists/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CombinatingLists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Dai chisla: ");
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            int number = nums[nums.Count - 1];
+            while (nums.Contains(number))
+            {
+                nums.Remove(number);
+            }
+            Console.WriteLine(string.Join(", ", nums));
+        }
+    }
+}
